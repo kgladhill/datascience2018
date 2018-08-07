44bbac236b44bf1c8bad2b2d2fdbe79e347aa367
kgladhill <kristie.w.gladhill@ododt.state.or.us>
2018-08-07 15:11:22 -0700

    commit new projecct note

---
+++ code/git_newProject.R
@@ -0,0 +1,26 @@
+######### ######### ######### ######### ######### 
+### the following three lines are to be typed in terminal window
+#  Tools -> Shell
+# git config --global user.name "kgladhill"
+# git config --global user.email "kristie.w.gladhill@odot.state.or.us"
+# git config --global color.ui "auto"
+
+# click in Git window, upper right corner
+# click which files you want "staged"
+
+#initiate       # only need to create a repository once (ever)
+#add
+#commit a file  # adds a point in the file history
+#               #   each commit is saved, can move back to earlier version in history
+# status        # checks for difference between versions
+# log
+# diff          # allows comparison bewteen earlier committed versions and current version
+# revert        # allows user to revert to an earlier version
+# checkout
+# push          # commit to gitHub repository
+# pull          # synchronizing remote version with current version
+# pull request  # a way for external folks to collaborate with you
+#               #   without giving them write access to your repository
+
+#see if these edits get onto web repository
+# new project
